1e3faaade057d708144fa84a00fabfa570c5b715
SpatiallyAbsent <joshuapdow@gmail.com>
2024-02-06 14:39:19 -0700

    added data to User.js, Post.js, and comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,43 @@
-const 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserComment extends Model { }
+
+UserComment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+
+    },
+ {
+    sequelize,
+    modelName: 'user_comment',
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+});
+
+module.exports = UserComment;
