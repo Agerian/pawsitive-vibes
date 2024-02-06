073962490f54d0b54fc811cfe3d35f9e7c499c2e
SpatiallyAbsent <joshuapdow@gmail.com>
2024-02-06 15:03:25 -0700

    added Photo.js (will still need more work) and User.js in models

--- models/Photo.js
+++ models/Photo.js
@@ -5,7 +5,40 @@ class Photo extends Model { }
 
 Photo.init({
     id: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    //add other needed attributes here. Need to get with Daniel for this one.
+    /* below is example of url: (may not need at all)
+        url: {
+        type: DataTypes.STRING,
+        allowNull: false
+    */
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
     }
-})
+}, {
+    sequelize,
+    modelName: 'photo',
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+});
+
+module.exports = Photo;
 
