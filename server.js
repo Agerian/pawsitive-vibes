cfc88e8f72b274945db2121249ead01e7399443f
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-09 10:42:12 -0700

    Add Nav and signup form

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
 
-//Makes the app listen to port 3000
+
+//Makes the app listen to port 3001
 app.listen(port, () => console.log(`App listening to port ${port}`));
