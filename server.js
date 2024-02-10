4b988a1439926648574cd1cf6e9223c2e93a7cf6
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-10 13:54:58 -0700

    Add API routes for user signup, login, and logout (Complete)

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 // Loads the sequelize module
 const sequelize = require('./config/connection');
-const bcrypt = require('bcrypt');
-const withAuth = require('./utils/auth');
-
+// Loads the routes module
+const routes = require('./controller');
 
 // Loads the session module
 const session = require('express-session');
@@ -51,112 +50,8 @@ app.use(express.urlencoded({ extended: false }));
 //Serves static files (we need it to import a css file)
 app.use(express.static('public'))
 
-//Logs the request method, the requested URL, and the time it took to process the request
-// Temporary for diagnostics
-app.use((req, res, next) => {
-  console.log("complete Raw Body:", req.body, typeof req.body);
-  next();
-})
-
-
-//Sets a basic route
-app.get('/', (req, res) => {
-  loggedIn = req.session.loggedIn;
-    res.render('home', {loggedIn});
-});
-
-app.get('/signup', (req, res) => {
-    res.render('signup');
-});
-
-app.get('/dashboard', (req, res) => {
-  res.render('dashboard');
-});
-
-app.post('/api/signup', async (req, res) => {
-  console.log("Recieved User Body:", req.body);
-  try {
-    const userData = await User.create(req.body);
-
-    // Start a session for the newly created user
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.loggedIn = true;
-      res.status(200).json(userData);
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// GET LOGIN
-// GET the login page ('/login')
-app.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-  }
-
-  res.render('login');
-});
-
-
-// LOGIN
-// User Login ('/api/user/login')
-app.post('/api/login', async (req, res) => {
-  try {
-      console.log('Login request body:', req.body);
-      const user = await User.findOne({
-          where: {
-              email: req.body.email,
-          },
-      });
-
-      if (!user) {
-          res.status(400).json({ message: 'Incorrect username or password, please try again' });
-          return;
-      }
-
-      const validPassword = await bcrypt.compare(req.body.password, user.password);
-
-      if (!validPassword) {
-          res.status(400).json({ message: 'Incorrect username or password, please try again' });
-          return;
-      }
-
-      req.session.save(() => {
-          req.session.user_id = user.id;
-          req.session.username = user.username;
-          req.session.loggedIn = true;
-
-          console.log('User logged in successfully');
-          console.log('Session:', req.session);
-          res.redirect('/');
-      });
-  } catch (err) {
-      console.log('Login error:', err);
-      res.status(500).json({ error: 'An error occurred while logging in' });
-  }
-});
-
-//Logout
-// User Logout ('/api/user/logout')
-app.post('/api/logout', async (req, res) => {
-  console.log('Logout request recieved:', req.session)
-  if (req.session.loggedIn) {
-//      console.log('Destroying session:', req.session.id)
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    console.log('No session to destroy')
-    res.status(404).end();
-  }
-});
-
-
-
+// Sets the app to use the routes in the routes.js file
+app.use(routes);
 
 //Makes the app listen to port 3001
 app.listen(port, () => console.log(`App listening to port ${port}`));
