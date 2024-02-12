13deb5e605a039bdf665647a1619be61036adb3d
SpatiallyAbsent <joshuapdow@gmail.com>
2024-02-12 15:20:51 -0700

    finishing touches so far

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,8 @@
+DROP DATABASE IF EXISTS pawsitive_vibes_db;
 
+CREATE DATABASE pawsitive_vibes_db;
+
+USE pawsitive_vibes_db;
 
 -- Create the user table
 /*CREATE TABLE pawsitive_vibes.user (
@@ -62,4 +66,37 @@ VALUES ('Beautiful', 'Beautiful Dog', 1);
 ('Unattractive', 'Sorry But No', 10);
 ('Hideous', 'Hope they keep you inside', 11);
 ('Scruffy', 'Hey who you calling scruffy looking?', 12);*/
+
+-- Create the user table
+/*CREATE TABLE user (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(50) NOT NULL,
+ 
+);
+
+-- Create the post table
+CREATE TABLE post (
+  id INT PRIMARY KEY, -- a unique identifier for each post
+  title VARCHAR(255) NOT NULL, -- the title of the post
+  content TEXT NOT NULL, -- the content of the post
+  user_id INT NOT NULL, -- the id of the author who wrote the post
+  date DATE NULL, -- the date when the post was published
+  category VARCHAR(255), -- the category of the post, such as dog, cat, etc.
+  FOREIGN KEY (author_id) REFERENCES author(id) -- a reference to the author table
+);
+
+
+-- Create the comment table
+CREATE TABLE comment (
+  comment_id INT PRIMARY KEY,
+  user_id INT NOT NULL,
+  service_id INT NOT NULL,
+  rating INT CHECK (rating BETWEEN 1 AND 5),
+  feedback TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES user (user_id),
+  FOREIGN KEY (service_id) REFERENCES service (service_id)
+);*/
 
